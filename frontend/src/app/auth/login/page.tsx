2844ef63f2523932bf5ab335c95205030d877a8e
MaleeshaMadhuhansani99 <maleeshamapalagama@gmail.com>
2026-07-20 13:25:51 +0530

    redux setup

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -24,5 +24,5 @@ export default function LoginPage() {
         <RegisterOrganizationBtn />
       </div>
     </div>
-  );
+  ); 
 }
--- frontend/src/component/buttons/register-organization-btn.tsx
+++ frontend/src/component/buttons/register-organization-btn.tsx
@@ -1,16 +1,48 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/src/store/hooks";
+import { loginSuccess } from "@/src/store/slices/authSlice";
 import { useRouter } from "next/navigation";
 
 export default function RegisterOrganizationBtn() {
   const router = useRouter();
 
+const dispatch = useAppDispatch();
+
+  const auth = useAppSelector((state) => state.auth);
+
+  function userData() {
+    console.log("User Data:", auth.user);
+    console.log("Token:", auth.token);
+    console.log("Is Authenticated:", auth.isAuthenticated);
+  }
+
+  function handleLogin() {
+    dispatch(
+      loginSuccess({
+        user: {
+          id: "1",
+          email: "test@test.com",
+          role: "ADMIN",
+        },
+        token: "abc123",
+      })
+    );
+    userData();
+  }
+
   return (
+    // <button
+    //   className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700 transition-colors"
+    //   onClick={() => router.push("/auth/register-organization")}
+    // >
+    //   Register Organization
+    // </button>
     <button
-      className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700 transition-colors"
-      onClick={() => router.push("/auth/register-organization")}
+        onClick={handleLogin}    
+        className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700 transition-colors"
     >
-      Register Organization
-    </button>
+        Register Organization
+      </button> 
   );
 }
